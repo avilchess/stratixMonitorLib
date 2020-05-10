468f1cb415d559f81d16a5f808f3a12dc1e3999d
Antonio Vilches <antonio.vilches@shapelets.io>
2020-05-10 13:23:11 +0200

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -10,18 +10,18 @@
 
 int main (int argc, char *argv[]) {
 
-    SML * sml = SML::getInstance(100);
+    StratixMonitor * StratixMonitor = StratixMonitor::getInstance(100);
 
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    auto counter1 = sml->get_hardware_counters();
+    auto counter1 = StratixMonitor->get_hardware_counters();
     std::cout << "Counter1: " << counter1.getCounters()[0] << std::endl;
 
     std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-    auto counter2 = sml->get_hardware_counters();
+    auto counter2 = StratixMonitor->get_hardware_counters();
     std::cout << "Counter2: " << counter2.getCounters()[0] << std::endl;
 
     std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-    auto counter3 = sml->get_hardware_counters();
+    auto counter3 = StratixMonitor->get_hardware_counters();
     std::cout << "Counter3: " << counter3.getCounters()[0] << std::endl;
 
     return 0;
